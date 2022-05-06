df9e4d41700b7e1ddf9ad01b617f9ff78e3102a3
Londort <landort@gmail.com>
2022-05-06 20:38:42 +0200

    task 3.1 complete

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,21 @@
 const stream = require('stream');
+const { Chunk } = require('webpack');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.buf = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.buf += chunk.length;
+    if (this.buf > this.limit){
+      callback(new LimitExceededError());
+    }else {
+      callback(null, chunk);
+    }
   }
 }
 
